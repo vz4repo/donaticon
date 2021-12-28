62fc00b4b10db6fb4f7510ed41f85637ea03c353
vz-mac <thevzforalt@gmail.com>
2021-12-28 16:29:28 +0900

    initial commit

---
+++ src/main/java/com/project/donaticon/DonaticonApplication.java
@@ -0,0 +1,13 @@
+package com.project.donaticon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonaticonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonaticonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/donaticon/DonaticonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.donaticon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonaticonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
